fdc97929beb47fb5c692f01b053e82ecae4dd7d7
cvnd <claudiavdioneda@gmail.com>
2021-05-14 15:10:44 -0400

    clear out mass comment blocks

--- main.js
+++ main.js
@@ -6,11 +6,8 @@ const commands = require('./commands.json');
 var config = require('./config.json');
 const { WebhookClient, MessageAttachment } = require('discord.js');
 
-// create a new Discord client
 const client = new Discord.Client({ partials: ['MESSAGE', 'CHANNEL', 'REACTION'] });
 
-// when the client is ready, run this code
-// this event will only trigger one time after logging in
 client.once('ready', () => {
 
     console.log('Ready!');
@@ -30,18 +27,12 @@ client.once('ready', () => {
             const args = interaction.data.options;
             const sender = interaction.member;
     
-            //let exists = await DB.checkGuild(guild_id);
             var guild_settings = await fetchSettings(guild_id);
 
 
             checkSettings(guild_settings);
     
             if(!guild_settings.set) {
-                // Do things
-                //updateSettings(interaction, 'interaction');
-                //console.log("NOT SET");
-
-                //failedInteraction("This bot is not fully configured for your server. Use `!trix help` to view how to get started.")
                 client.api.interactions(interaction.id, interaction.token).callback.post({
                     data: {
                         type: 3,
@@ -54,10 +45,8 @@ client.once('ready', () => {
                 return;
             }
 
-            //console.log(interaction);
 
             if(guild_settings.commands_channel != interaction.channel_id) {
-                //failedInteraction("Commands are disabled for this channel. Please visit #" + guild_settings.commands_channel.name + " instead")
                 client.api.interactions(interaction.id, interaction.token).callback.post({
                     data: {
                         type: 3,
@@ -72,17 +61,6 @@ client.once('ready', () => {
             // Create embed
             const mail = createMail(args, sender, interaction.id);
 
-            // Insert info into DB
-            
-
-        
-            //console.log(client.users.fetch(sender.user.id));
-            // guild_settings.inbox_channel.send(mail).then(msg=>{
-            //     DB.insertMessages(interaction, msg.id);
-            //     DB.insertReply(interaction.id, msg.id, sender.user.id);
-            //     //msg.react('✉️');
-            //     //DB.postedMessage(interaction.id, msg.id);
-            // });
             client.guilds.cache.get(guild_id).channels.cache.get(guild_settings.inbox_channel).send({embed: mail}).then(resp=>DB.insertInteraction(interaction, resp.id));    
             DB.insertGlobalInteraction(interaction.id, guild_id);
             client.api.interactions(interaction.id, interaction.token).callback.post({
@@ -106,39 +84,8 @@ client.once('ready', () => {
         
         }
     });
-
-    client.on('messageReactionAdd', async (reaction, user) => {
-
-        //console.log(reaction);
-        // When we receive a reaction we check if the reaction is partial or not
-        if (reaction.partial) {
-            // If the message this reaction belongs to was removed the fetching might result in an API error, which we need to handle
-            try {
-                await reaction.fetch();
-            } catch (error) {
-                console.error('Something went wrong when fetching the message: ', error);
-                // Return as `reaction.message.author` may be undefined/null
-                return;
-            }
-        }
-        // Now the message has been cached and is fully available
-        //console.log(`${reaction.message.author}'s message "${reaction.message.content}" gained a reaction!`);
-        // The reaction is now also fully available and the properties will be reflected accurately:
-        //console.log(`${reaction.count} user(s) have given the same reaction to this message!`);
-        //const msg_id = reaction.message.id;
-        //const interaction = DB.getInteraction(msg_id, client.channels.cache.get('822859340812910592'));
-        //console.log(interaction);
-        if(user.bot === false ) {
-            // const msg = '```Mail Code: '+ interaction +'```';
-            // console.log(msg);
-            // DB.getInteraction(msg_id, client.channels.cache.get('822859340812910592'));
-            // client.channels.cache.get('822859340812910592').send(msg);
-        }
-    });
     
     client.on('message', async function(message){
-        //console.log(message);
-        // If message is from user and if it's in a guild channel
 
      
         const sender = message.author;
@@ -146,10 +93,7 @@ client.once('ready', () => {
         var userhandle = sender.username + '#' +  sender.discriminator;
         //console.log(message.channel);
         // If message is a direct reply
-    
-        //console.log(message.author);
         let user = client.users.fetch(message.author.id);
-        //console.log(user);
         if(message.reference !== null && message.author.bot !== true) {
 
             // Get ID of message it's replying to (replied_message)
@@ -334,27 +278,12 @@ client.once('ready', () => {
                 message.channel.send("Sorry, this command is not recognized. Please use `!trix help` to view usable commands.");
             }
 
-            //console.log(guild_settings);
             checkSettings(guild_settings);
-            // if(isSet(guild_settings)) {
-            //     guild_settings.set = true;
-            // }
         } 
     });
     
 });
 
-// function updateSettings(event, type) {
-//     console.log(event);
-//     var channel_id = '';
-//     if(type == 'interaction') {
-//         channel_id = event.channel_id;
-//     } else if (type == 'message') {
-//         channel_id = event.channel.id;
-//     }
-//     console.log(channel_id);
-//     client.channel.cache.get(channel_id).send('Thank you for installing Trix! To start setting configuration, please type `!trix inbox [channel_name]` .');  
-// }
 
 function checkChannelPermissions(channel, purpose) {
     var permissions = []
@@ -362,10 +291,11 @@ function checkChannelPermissions(channel, purpose) {
         permissions = ['']
     }
 }
+
 function createMail(args, sender, id) {
     var user_id = '';
     var pfp ='';
-    //console.log(args);
+
     // If the mode value is set and is set to public
     if(args.length == 2 && args[1].value == 'public') {
         user_id += sender.user.username + '#' + sender.user.discriminator; 
@@ -377,7 +307,7 @@ function createMail(args, sender, id) {
         pfp = config.anon_avatar;
     }
 
-    const msg = new Discord.MessageEmbed()
+    return new Discord.MessageEmbed()
                 .setColor(config.member_color)
                 .setTitle("Submission #" + truncateInteractionID(id))
                 .setAuthor(user_id, pfp)
@@ -388,7 +318,6 @@ function createMail(args, sender, id) {
                 .setTimestamp()
                 .setFooter(config.mail_footer);
 
-    return msg;
 }
 
 function truncateInteractionID(id) {
@@ -400,52 +329,22 @@ function truncateAuthorID(id) {
 }
 
 async function fetchSettings(guild_id) {
-    // var guild_settings = {};
     // Check if guild exists in table
     var exists = await DB.checkGuildExists(guild_id);
     //console.log(exists);
 
     if(exists) {
 
-        // If it does exist, fetch settings
         var settings = await DB.getGuildSettings(guild_id);
-        // .then(settings=>{
-        //     //console.log(settings);
-        //     if(isSet(settings).length === 0) {
-        //         guild_settings.patreon_tier = settings.patreon_tier;
-        //         guild_settings.commands_channel = client.channels.cache.get(settings.commands_channel);
-        //         guild_settings.inbox_channel = client.channels.cache.get(settings.inbox_channel);
-        //         guild_settings.default_mode = settings.default_mode;
-        //         guild_settings.set = true;
-        //     }
-            
-        //     //console.log(guild_settings);
-        // });
     } else {
         DB.createGuildSettings(guild_id);
         DB.createRepliesTable(guild_id);
         DB.createInteractionssTable(guild_id);
         var settings = await DB.getGuildSettings(guild_id);
-        // .then(settings=>{
-        //     //console.log(settings);
-        //     if(isSet(settings).length === 0) {
-        //         guild_settings.patreon_tier = settings.patreon_tier;
-        //         guild_settings.commands_channel = client.channels.cache.get(settings.commands_channel);
-        //         guild_settings.inbox_channel = client.channels.cache.get(settings.inbox_channel);
-        //         guild_settings.default_mode = settings.default_mode;
-        //         guild_settings.set = true;
-        //     }
-            
-        //     //console.log(guild_settings);
-        // });
     }
-    //console.log(settings.inbox_channel);
     return settings;
 }
 
-// function getGuildSettings(guild_id, settings) {
-    
-// }
 
 function isSet(vals) {
     var set = true;
